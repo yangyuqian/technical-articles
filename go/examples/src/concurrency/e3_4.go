dbad27ae869a4c5c2fe13c58c637ccb73fc770e3
Yuqian Yang <yyang@freewheel.tv>
2016-10-02 22:40:11 +0800

    fix bugs in e3_3.go

--- go/examples/src/concurrency/e3_4.go
+++ go/examples/src/concurrency/e3_4.go
@@ -15,32 +15,18 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	runtime.GOMAXPROCS(2)
 	var wg sync.WaitGroup
-	tranChan := make(chan bool)
 	balance = 1000
 	transactionNo = 0
 	fmt.Println("Starting balance: $", balance)
-	wg.Add(1)
+	wg.Add(100)
 	for i := 0; i < 100; i++ {
-		go func(ii int, trChan chan (bool)) {
+		go func(ii int, sig *sync.WaitGroup) {
 			transactionAmount := rand.Intn(25)
 			transaction(transactionAmount)
-			if ii == 99 {
-				trChan <- true
-
-			}
-
-		}(i, tranChan)
-
-	}
-	go transaction(0)
-	select {
-	case <-tranChan:
-		fmt.Println("Transactions finished")
-		wg.Done()
-
+			wg.Done()
+		}(i, &wg)
 	}
 	wg.Wait()
-	close(tranChan)
 	fmt.Println("Final balance: $", balance)
 }
 
