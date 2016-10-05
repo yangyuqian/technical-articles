cb13a8a2890f0a9c4020362ca02d7651d6a28cb1
Yuqian Yang <yyang@freewheel.tv>
2016-10-05 20:48:22 +0800

    add comments for GOMAXPROCS and NumCPU

--- go/examples/src/concurrency/e3_6.go
+++ go/examples/src/concurrency/e3_6.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"net/http"
-	"net/http/pprof"
+	_ "net/http/pprof"
+	"runtime"
 	"time"
 )
 
 func main() {
+	runtime.GOMAXPROCS(1)
+	println("logic CPU:", runtime.NumCPU())
 	http.HandleFunc("/", Hello)
-	http.HandleFunc("/debug/xxx", pprof.Index)
 	http.ListenAndServe(":8080", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(20 * time.Second)
+	time.Sleep(2 * time.Second)
 }
