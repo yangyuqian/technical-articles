5cacbe9ca8c81496b3957e410f7d4cee5be1da47
Yuqian Yang <yyang@freewheel.tv>
2016-10-07 21:49:51 +0800

    add comments for pointer type channel example

--- go/examples/src/concurrency/chan_pointer_type.go
+++ go/examples/src/concurrency/chan_pointer_type.go
@@ -5,6 +5,10 @@ import "sync"
 /*
 	Channel中可以传递指针类型，而且不同的goroutine之间其实是共享栈空间的，
 	也就意味着指针在不同routine之间是等价的.
+
+	在Channel中传递指针可行但不推荐，因为Channel的目的在于减少不必要的共享内存，
+	即goroutine A中直接修改goroutine B中正在使用的内存，如果在channel中传递了
+	指针，性能会变得更差，但却没有带来额外的好处，和传统的多线程编程就没有区别了
 */
 
 func main() {
